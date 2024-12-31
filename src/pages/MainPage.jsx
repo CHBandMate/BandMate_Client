535d6209bc06c7d6ddbc06d09acaa8e365cc06e8
harry <rnd903@gmail.com>
2024-12-31 11:21:46 +0900

    241231 main 작업중

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -9,9 +9,19 @@ function MainPage() {
         <div className="inner">
           <div className="gnb">
             <ul>
-              <li className="login">로그인/회원가입</li>
-              <li>마이페이지</li>
-              <li>로그아웃</li>
+              <li className="login none">로그인/회원가입</li>
+              <li className="user">
+                <div className="img">
+                  <img src="/src/assets/images/icon/user-avatar.png" alt="" />
+                </div>
+                마이페이지
+              </li>
+              <li className="logout">
+                <div className="img">
+                  <img src="/src/assets/images/icon/logout.png" alt="" />
+                </div>
+                로그아웃
+              </li>
             </ul>
           </div>
           <div className="gnb_menu">
