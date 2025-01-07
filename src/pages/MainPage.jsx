3f63edfbfc8a03acded782c90ff69f21fa585c9b
harry <rnd903@gmail.com>
2025-01-07 17:13:32 +0900

    250107 main 1차 완료

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -14,6 +14,35 @@ function MainPage() {
           <div className="band-item-area">
             <div className="band-item-card">
               <div className="item-card-img">
+                <p>TEST_Band2</p>
+              </div>
+              <div className="item-card-txt">
+                <div className="item-box item-card-user">
+                  <div className="user-txt">
+                    <p className="user-name">TEST_Band2</p>
+                    <span className="user-category">클래식</span>
+                  </div>
+                  <div className="user-bmark">
+                    <img src="/src/assets/images/icon/bookmark.png" alt="" />
+                  </div>
+                </div>
+                <div className="item-box item-card-title">
+                  <p className="title">같이 연주하실 분~!!</p>
+                </div>
+                <div className="item-box item-card-skill">
+                  <div className="skill-item">#통 기타</div>
+                  <div className="skill-item">#피아노</div>
+                </div>
+                <div className="item-box item-card-location">
+                  <div className="location_icon">
+                    <img src="/src/assets/images/icon/pin.png" alt="" />
+                  </div>
+                  <p>서울시 중구</p>
+                </div>
+              </div>
+            </div>
+            <div className="band-item-card">
+              <div className="item-card-img">
                 <p>TEST_Band</p>
               </div>
               <div className="item-card-txt">
