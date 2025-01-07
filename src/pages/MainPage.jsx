b448ad9e2cfc3770df45080ed63efb8ad5d9e933
harry <rnd903@gmail.com>
2025-01-07 16:21:36 +0900

    250107 main 퍼블 작업 진행중...

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -11,6 +11,41 @@ function MainPage() {
       <div className="main-content">
         <div className="inner">
           <SearchBox />
+          <div className="band-item-area">
+            <div className="band-item-card">
+              <div className="item-card-img">
+                <p>TEST_Band</p>
+              </div>
+              <div className="item-card-txt">
+                <div className="item-box item-card-user">
+                  <div className="user-txt">
+                    <p className="user-name">TEST_Band</p>
+                    <span className="user-category">재즈</span>
+                  </div>
+                  <div className="user-bmark">
+                    <img
+                      src="/src/assets/images/icon/bookmark_active.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="item-box item-card-title">
+                  <p className="title">밴드 보컬 구합니다.</p>
+                </div>
+                <div className="item-box item-card-skill">
+                  <div className="skill-item">#일렉기타</div>
+                  <div className="skill-item">#베이스</div>
+                  <div className="skill-item">#피아노</div>
+                </div>
+                <div className="item-box item-card-location">
+                  <div className="location_icon">
+                    <img src="/src/assets/images/icon/pin.png" alt="" />
+                  </div>
+                  <p>서울시 강남구</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
