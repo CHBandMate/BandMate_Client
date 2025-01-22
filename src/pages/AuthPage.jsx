f31101316f97cdee00ad3b7f160b45837d7bca63
harry <rnd903@gmail.com>
2025-01-22 15:08:49 +0900

    rerebase

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchToken } from "../redux/authslice";
@@ -18,5 +19,42 @@ const AuthPage = () => {
 
   return <div>로그인 중...</div>;
 };
+=======
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+// http://localhost:3000/auth?identifier=8&code=6228c0a1-bdb0-4298-87f6-b53dba37018d
+function AuthPage() {
+  const navigate = useNavigate();
+  // 현재 URL 로그
+  const PARAMS = new URL(window.location.href).searchParams;
+  console.log(window.location.href);
+  // URL에서 identifier 추출
+  const identifier = PARAMS.get("identifier");
+  // URL에서 code 추출
+  const code = PARAMS.get("code");
+
+  useEffect(() => {
+    axios
+      .post(
+        `${
+          import.meta.env.VITE_REDIRECT_URL
+        }/auth?identifier=${identifier}&code=${code}`
+      )
+      .then((res) => {
+        console.log("res: ", res.data);
+
+        localStorage.setItem("token", res.data.token);
+        navigate("/loginSuccess");
+      });
+  }, []);
+
+  return (
+    <>
+      <div>로그인 중...</div>
+    </>
+  );
+}
+>>>>>>> 2866291 (250117 토근 발급 진행중)
 
 export default AuthPage;
---
+++ src/pages/loginSuccess.jsx
@@ -0,0 +1,4 @@
+function loginSuccess() {
+  return <>로그인 성공! 로그인 테스트중</>;
+}
+export default loginSuccess;
